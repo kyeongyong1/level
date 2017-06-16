fea85259aa101ad816754e29946d04a285b7bb99
Gaem <kyeongyong1@coupang.com>
2017-06-16 21:41:38 +0900

    init

--- KILevel/ViewController.swift
+++ KILevel/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    let motionManager = CMMotionManager()
+    
+    @IBOutlet weak var rotX: UILabel?
+    @IBOutlet weak var rotY: UILabel?
+    
+    @IBOutlet weak var XbarImage: UIImageView?
+ 
+    @IBOutlet weak var YbarImage: UIImageView?
+    @IBOutlet weak var XYBarImage: UIImageView?
+    
+    var XbarRect: CGRect?
+    var YbarRect: CGRect?
+    var XYbarRect: CGRect?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        XbarRect = XbarImage?.frame
+        YbarRect = YbarImage?.frame
+        XYbarRect = XYBarImage?.frame
+        
+        motionStart()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func motionStart() {
+    
+        motionManager.deviceMotionUpdateInterval = 0.01
+        
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: { (deviceMotion, error) -> Void in
+            
+            if error == nil {
+                if let myMotion = deviceMotion {
+                    let attitude = myMotion.attitude
+        
+                    self.displayXY(attitude: attitude)
+                    self.arrangeFrame(attitude: attitude)
+                }
+                
+            } else {
+                
+            }
+            
+            
+        })
+     
+    }
+    
+    func displayXY(attitude: CMAttitude) {
+        self.rotX?.text = "\(Double(round(attitude.roll * 100000) / 100000))"
+        self.rotY?.text = "\(Double(round(attitude.pitch * 100000) / 100000))"
+    }
+    
+    func arrangeFrame(attitude: CMAttitude) {
+        
+        let XBarImageCurrentY = XbarRect?.origin.y
+        
+        XbarImage?.frame = CGRect(x: (XbarImage?.frame.origin.x)!, y: (XBarImageCurrentY! + CGFloat(round(attitude.pitch * 100000) / 10000 * 3.5)), width: (XbarImage?.frame.size.width)!, height: (XbarImage?.frame.size.height)!)
+        
+        let YBarImageCurrentX = YbarRect?.origin.x
+        
+        YbarImage?.frame = CGRect(x: YBarImageCurrentX! + CGFloat(round(attitude.roll * 100000) / 10000 * 3.5), y: (YbarImage?.frame.origin.y)!, width: (YbarImage?.frame.size.width)!, height: (YbarImage?.frame.size.height)!)
+        
+        let XYBarImageCurrentY = XYbarRect?.origin.y
+        let XYBarImageCurrentX = XYbarRect?.origin.x
+        
+        XYBarImage?.frame = CGRect(x: XYBarImageCurrentX! + CGFloat(round(attitude.roll * 100000) / 10000 * 3.5), y: (XYBarImageCurrentY! + CGFloat(round(attitude.pitch * 100000) / 10000 * 3.5)), width: (XYBarImage?.frame.size.width)!, height: (XYBarImage?.frame.size.height)!)
+        
+        
+        
+        
+       
+        
+    }
+    
 }
 
